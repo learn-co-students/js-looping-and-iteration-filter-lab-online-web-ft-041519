e68fc11488bdad7b8e090d5918fa59fc69d8daba
Alison Golden <alisong779@yahoo.com>
2019-07-30 18:38:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(collection, name){
+  const newArray = [];
+  for (const driver of collection) {
+    if (driver.toLowerCase() === name.toLowerCase()){
+      newArray.push(driver);
+    }
+  }
+  return newArray
+}
+
+function fuzzyMatch(collection, letters){
+  const newArray = [];
+  for (const driver of collection){
+    if (driver.match(`^${letters}`)){
+      newArray.push(driver);
+    }
+  }
+  return newArray
+}
+
+function matchName(collection, name){
+  const newArray = [];
+  for (const driver of collection){
+    if (driver.name.toLowerCase() === name.toLowerCase()){
+      newArray.push(driver);
+    }
+  }
+  return newArray
+}

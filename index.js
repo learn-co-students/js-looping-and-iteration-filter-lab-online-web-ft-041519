a9b9d225a9a393c09b2dec2bbd26351ec1e804a5
Kish-Siva-1 <rodes88@msn.com>
2019-08-03 03:37:06 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+function findMatching(arrayse, names) {
+    
+    return arrayse.filter(function (num) { return num.toUpperCase() === names.toUpperCase(); });
+    
+}
+
+function fuzzyMatch(arrayse, names) {
+
+    let partial = names.length
+
+    return arrayse.filter(function (num) { 
+
+        return num.slice(0, partial).toUpperCase() === names.toUpperCase(); });
+
+}
+
+function matchName(arrayse, names) {
+    return arrayse.filter(function (num) { return num.name === names; });
+}
